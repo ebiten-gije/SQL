cd7b60e0963213826c7f52c6750a08afb040e0cc
ebiten-gije <gije08@gmail.com>
2024-04-26 12:17:21 +0900

    240426 중간

---
+++ Practice02.sql
@@ -0,0 +1,33 @@
+--  01
+select 
+count(manager_id) "haveMngCnt"
+from employees
+;
+
+--  02
+select max(salary), min(salary)
+from employees;
+
+--  03
+select max(hire_date)
+from employees;
+
+--  04
+
+
+--  05
+
+
+--  06
+
+
+--  07
+
+
+--  08
+
+
+--  09
+
+
+--  10

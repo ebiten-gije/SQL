0e6ca422e0a528eb24eee0964f5a0f641c223a9f
ebiten-gije <gije08@gmail.com>
2024-04-29 17:49:48 +0900

    240429

--- 02-DML-JOIN-AGGREGATION.sql
+++ 02-DML-JOIN-AGGREGATION.sql
@@ -118,3 +118,164 @@ select emp.employee_id, emp.first_name,
 from employees emp, employees man
 where emp.manager_id = man.employee_id(+);
 
+
+---------------------------
+--  GROUP AGGREGATION
+-----------------------------
+
+--  집계: 여러 행으로부터 데이터를 수집, 하나의 행으로 반환
+
+--  count: 갯수 세기 함수
+--  employees 테이블의 총 레코드 갯수:
+select count(*) from employees;
+
+--  * 로 카운트하면 모든 행의 수를 반환
+--  특정 컬럼 내에 null값의 포함여부는 중요치 않음
+
+--  commission 받는 직원의 수를 알고 싶을 경우
+--  commission_pct가 null인 경우를 제외하고 싶다면;
+select count (commission_pct) from employees;
+--  컬럼 내에 포함된 null 데이터를 카운트하지 않음
+
+--  위 쿼리는 아래와 같다
+select count (*) from employees
+where commission_pct is not null;
+
+--  sum : 합계함수
+--  모든 사원의 급여의 합계
+select sum (salary) from employees;
+
+--  avg: 평균 함수
+--  사원들의 평균 급여?
+select avg(salary) from employees;
+
+--  사원들의 커미션 비율 평균은?
+select avg (commission_pct) from employees; --  0.222
+--  avg 함수는 null값이 포함되어있을 경우, 그 값을 집계 수치에서 제외
+--   null값을 집계 결과에 포함시킬지의 여부는 정책으로 결정하고 수행해야 한다
+select avg (NVL(commission_pct, 0)) from employees; --  0.072
+
+--  min / max: 최솟값 / 최댓값
+--  avg / median: 산술평균 / 중앙값
+select 
+    min(salary),
+    max(salary),
+    avg(salary),
+    median(salary)
+from employees;
+
+--  흔히 범하는 오류ㄷㄷ
+--  부서별 평균 급여를 구하고 싶을 때,
+select department_id, avg(salary)
+from employees;
+
+select department_id from employees;    --  여러개의 레코드
+select avg(salary) from employees;      --  단일 레코드
+
+select department_id, salary
+from employees
+order by department_id;
+
+--  GROUP by
+select department_id, round (avg(salary), 2)
+from employees
+group by department_id      --  집계 위해 특정 컬럼 기준으로 그룹핑
+order by department_id;
+
+--  부서별 평균 급여에 부서명도 포함하여 출력
+select emp.department_id, dep.department_name, round( AVG(salary), 0)
+from employees emp join departments dep on emp.department_id = dep.department_id
+group by emp.department_id, dep.department_name
+order by emp.department_id;
+
+--  GROUP by 절 이후에는 GROUP BY 에 참여한 컬럼과 집계함수만 남는다
+
+
+--  평균 급여가 7000 이상인 부서만 출력해보기
+select department_id, avg(salary)
+from employees
+where avg(salary) >= 7000   --  아직 집계함수 시행되지 않은 상태, 집계함수의 비교 불가
+group by department_id
+order by department_id;
+
+--  집계 함수 이후의 조건 비교 => having 절을 이용
+select department_id, avg(salary)
+from employees
+group by department_id
+having avg(salary) >= 7000  --  GROUP bY AGGREGATION의 조건 필터링
+order by department_id;
+
+
+--  ROLLUP
+--  GROUP by 절과 함꼐 사용
+--  그룹 지어진 결과에 대한 좀 더 상세한 요약을 제공하는 기능 수행
+--  일종의 Item Total
+select department_id, job_id, sum(salary)
+from employees
+group by rollup (department_id, job_id);
+
+
+--  CUBE
+--  CrossTab에 대한 Summary를 함께 추출하는 함수
+--  RollUp 함수에 의해 출력되는 Item Total 값과 함께
+--  Columm Total값을 함께 출력
+select department_id, job_id, sum(salary)
+from employees
+group by cube (department_id, job_id)
+order by department_id;
+
+
+---------------------------------
+--  SUBQUERY
+-------------------------------
+
+--  모든 직원 급여의 중앙값보다 많은 급여를 받는 사원의 목록
+--  1) 급여의 중앙값?
+--  2) 1번보다 많은 급여를 받는 직원의 목록
+
+--  1)
+select median (Salary) from employees;  --  6200
+
+--  2) 1번 결과 (6200) 보다 많은 급여를 받는 직원의 목록
+select first_name, salary
+from employees
+where salary >= 6200;
+
+--  1) 과 2) 쿼리 합치기
+select first_name, salary
+from employees
+where salary >= (select median (Salary) from employees)
+order by salary desc;
+
+--  Susan 보다 늦게 입사한 사람의 정보
+--  1) susan의 입사일
+--  2) 1)보다 늦게 입사한 사원의 정보
+
+--  1) Susan으ㅟ 입사일
+select hire_date
+from employees
+where first_name = 'Susan';
+
+--  2) 12/06/07보다 늦게 입사한 사람
+select first_name, hire_date
+from employees
+where hire_date > '12/06/07'
+order by hire_date;
+
+--  1) 2) 합치기
+select first_name, hire_date
+from employees
+where hire_date > (select hire_date
+from employees
+where first_name = 'Susan')
+order by hire_date;
+
+
+--  연습
+--  급여를 모든 직원 급여의 중앙값보다 많이 받기, 수잔보다 늦게 입사하기
+select first_name, salary, hire_date
+from employees
+where hire_date > (select hire_date from employees
+where first_name = 'Susan')
+and salary > (select median (Salary) from employees)
+order by salary, hire_date;
--- Practice02.sql
+++ Practice02.sql
@@ -13,22 +13,58 @@ select to_char(max(hire_date), 'YYYY"년" MM"월" DD"일"')
 from employees;
 
 --  04
-select employees.department_id
-from employees
+select employees.department_id, max(salary), min(salary), round (avg(salary),2)
+from employees join departments
+on employees.department_id = departments.department_id
+group by employees.department_id
 order by department_id desc;
 
 --  05
+select employees.job_id, max(salary), min(salary), round (avg(salary), 0)
+from employees join jobs
+on employees.job_id = jobs.job_id
+group by employees.job_id
+order by min(salary) desc, avg(salary);
 
 --  06
-
+select to_char(min(hire_date), 'YYYY-MM-DD DAY')
+from employees;
 
 --  07
-
+select employees.department_id, round (avg(salary),2) 평균임금, min(salary) 최저임금, 
+round (avg(salary),2) - min(salary) "평균임금 - 최저임금"
+from employees join departments
+on employees.department_id = departments.department_id
+group by employees.department_id
+having avg(salary) - min(salary) < 2000
+order by avg(salary) - min(salary) desc;
 
 --  08
-
+select job_title, max_salary - min_salary
+from jobs
+order by max_salary - min_salary desc ;
 
 --  09
-
+select manager_id--, round(avg(salary),1) as 평균임금, min(salary), max(salary)
+from employees
+where hire_date >= '15/01/01'
+group by manager_id
+having avg(salary) >= 5000
+order by avg(salary) desc;
 
 --  10
+select first_name, hire_date, 
+case 
+when hire_date < '12/12/31' then '창립맴버'
+when hire_date >= '13/01/01' and hire_date < '14/01/01' then '13년 입사'
+when hire_date >= '14/01/01' and hire_date < '15/01/01' then '14년 입사'
+else '상장이후입사'
+end as optDate
+from employees
+order by hire_date;
+
+
+select emp.employee_id "사번", emp.first_name "이름", emp.hire_date "입사일",
+    man.first_name "매니저 이름", man.hire_date "매니저 입사일"
+from employees emp, employees man, departments dept
+where emp.manager_id = man.employee_id and emp.department_id = dept.department_id(+);
